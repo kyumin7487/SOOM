563aa8800f67089675ba294a787a1ec473594dd0
박규민 <131234408+kyumin7487@users.noreply.github.com>
2025-06-08 19:20:13 +0900

    feat :: toast-container #1

---
+++ components/toast-container.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import ToastNotification from "./toast-notification"
+import { useToast } from "@/hooks/use-toast"
+import styles from "./toast-container.module.scss"
+
+export default function ToastContainer() {
+    const { toasts, removeToast } = useToast()
+
+    return (
+        <div className={styles.container}>
+            {toasts.map((toast) => (
+                <ToastNotification
+                    key={toast.id}
+                    message={toast.message}
+                    type={toast.type}
+                    duration={toast.duration}
+                    onClose={() => removeToast(toast.id)}
+                />
+            ))}
+        </div>
+    )
+}
